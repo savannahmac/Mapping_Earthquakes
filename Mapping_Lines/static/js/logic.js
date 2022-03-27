c8d135c3eabc26fb4ba0fa8a3fc03b710d2d08dd
unknown <savannahmcintyre10@gmail.com>
2022-03-27 16:16:05 -0400

    mapped airport lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,10 +3,26 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      40.7, -94.5
+      39.50, -98.35
     ],
-    zoom: 4
+    zoom: 5
   });
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [37.6213, -122.3790],
+  [30.1975, -97.6664],
+  [35.8801, -78.7880],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781],
+];
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+  color: "blue",
+  opacity: 0.5,
+  dashArray: "4",
+  weight: 4,
+}).addTo(map);
 // Get data from cities.js
 let cityData = cities;  
 // Loop through the cities array and create one marker for each city.
@@ -25,7 +41,7 @@ cityData.forEach(function(city) {
 });
   // We create the tile layer that will be the background of our map.
 // Using 'streets' because it is the street-level map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
